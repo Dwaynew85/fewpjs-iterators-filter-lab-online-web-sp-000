6fab3206af2f0b2f794805d595936c954070f5a7
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-10 18:32:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function findMatching(arr, name) {
 function fuzzyMatch(arr, name) {
   return arr.filter(x => x.slice(0, name.length).includes(name))
 };
+
+function matchName(arr, string) {
+  return arr.filter(x => x.name === string);
+};
